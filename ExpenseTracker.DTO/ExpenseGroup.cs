8ea90fb39b4d6486350cf85a6901b64a6853f5c4
Greg Goncharov <gregory.goncharov@gmail.com>
2016-11-14 14:25:00 -0500

    Adding files in progress

---
+++ ExpenseTracker.DTO/ExpenseGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ExpenseTracker.DTO
+{
+
+    public class ExpenseGroup
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int ExpenseGroupStatusId { get; set; }
+
+        public ICollection<Expense> Expenses { get; set; } 
+    }
+}
---
+++ ExpenseTracker.DTO/ExpenseGroupStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ExpenseTracker.DTO
+{
+    public class ExpenseGroupStatus
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+   
+    }
+}
